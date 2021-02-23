06562a9304e3cf69c675fb5a11951369c769d1a8
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-02-23 16:33:09 +0100

    done

--- app/javascript/components/hamburger.js
+++ app/javascript/components/hamburger.js
@@ -3,10 +3,10 @@ const hamburgerResponsive = () => {
   const menuToggle = document.querySelector('.hamburger');
   const showcase = document.querySelector('.Lpage');
 
-  menuToggle addEventListener('click', () => {
-    // menuToggle.classList.toggle('active');
-    // showcase.classList.toggle('active');
-    console.log("hello");
+  menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    showcase.classList.toggle('active');
+    // console.log("hello");
   })
 }
 
