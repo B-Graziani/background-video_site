e4a853cab1a3326f5e6cd686160e4d39c796bc34
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-02-23 11:47:15 +0100

    en cours

---
+++ app/javascript/components/hamburger.js
@@ -0,0 +1,15 @@
+const hamburgerResponsive = () => {
+
+  const menuToggle = document.querySelector('.hamburger');
+  const showcase = document.querySelector('.Lpage');
+
+  menuToggle addEventListener('click', () => {
+    // menuToggle.classList.toggle('active');
+    // showcase.classList.toggle('active');
+    console.log("hello");
+  })
+}
+
+export { hamburgerResponsive };
+
+
